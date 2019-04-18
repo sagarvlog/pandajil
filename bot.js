d66019e9193d869e41e601cb93c7463136198d03
sagarvlog <49345199+sagarvlog@users.noreply.github.com>
2019-04-18 18:21:45 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
+    if(message.author.id != "568325372379136000" && message.channel.id === "562459772310519809"){
         if(msg.startsWith('>', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
+            if(msg === prefix + "bang" && message.channel.id === "562459772310519809"){
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
             else{
